78135f677df75f94ae96bc47c04425a8768c0c22
Rudrika <rudrikadave20226@gmail.com>
2026-06-18 17:06:42 +0530

    fix the datatype issue

---
+++ database/migrations/2026_06_18_102252_update_data_type_of_hindi_and_gujarati_description_at_news_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->text('hindi_description')->nullable()->after('title');
+            $table->text('gujarati_description')->nullable()->after('hindi_description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->dropColumn('hindi_description');
+            $table->dropColumn('gujarati_description');
+        });
+    }
+};
